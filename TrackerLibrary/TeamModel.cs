72bf073fd19a6d61ee4944d4a9450bf7aa3b7547
vmackovic <61590033+vmackovic@users.noreply.github.com>
2022-03-29 17:48:21 +0200

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; }
+        public string TeamName { get; set; }
+
+        public TeamModel()
+        {
+            TeamMembers = new List<PersonModel>();
+        }
+
+
+    }
+}
